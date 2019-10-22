140cf9f03750b656c6d05bcd950d5814e8818343
chrischne <chrischne@gmail.com>
2019-10-22 14:23:01 +0200

    minor corrections

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-var temperature = 0;
+var pulse = 0;
+var interval = 0;
 var r = 0;
+var temperature =0;
 
 
 
@@ -7,19 +9,19 @@ function setup () {
   createCanvas(windowWidth, windowHeight);
 
   var client = mqtt.connect('mqtt://aeba5ae7:98e21bb6bccdb957@broker.shiftr.io', {
-    clientId: 'vizinviz-simple-circle'
+    clientId: 'p5-pulse-interval-gsr'
   });
   console.log('client', client);
 
   client.on('connect', function () {
     console.log('client has connected!');
-    client.subscribe('/temperature');
+    client.subscribe('/pulse/interval');
   });
 
   client.on('message', function (topic, message) {
     console.log('new message:', topic, message.toString());
     temperature = +message.toString();
-
+    temperature = 10;
   });
 }
 
